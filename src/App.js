3515ae2c359b0755edcf8825b4963130c2b3cc83
Lyudmila <milo4kamoya@gmail.com>
2022-10-20 21:39:14 -0700

    Fontawesome icons component added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMaximize, faFire } from '@fortawesome/free-solid-svg-icons'
 
 export default function App() {
   return (
     <div className="container">
       <div className='editor-container'>
         <div className='editor-header'>
-          <p>Editor</p>
-          <p>X</p>
+          <p><FontAwesomeIcon icon={faFire} /> Editor</p>
+          <p><FontAwesomeIcon icon={faMaximize} /></p>
         </div>
         <textarea id='editor' rows="4" cols="60"></textarea>
       </div>
-      
+
       <div className='preview-container'>
         <div className='preview-header'>
-        <p>Preview</p>
-          <p>X</p>
+        <p><FontAwesomeIcon icon={faFire} /> Preview</p>
+          <p><FontAwesomeIcon icon={faMaximize} /></p>
         </div>
         <div id='preview'></div>
       </div>
