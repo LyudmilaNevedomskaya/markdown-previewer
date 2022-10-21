bef5777b557717005c482f459b9bd36d739f0818
Lyudmila <milo4kamoya@gmail.com>
2022-10-20 20:46:25 -0700

    editor and preview container added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      Markdown Previewer
+    <div className="container">
+      <div className='editor-container'>
+        <div className='editor-header'>
+          <p>Editor</p>
+          <p>X</p>
+        </div>
+        <textarea id='editor' rows="4" cols="60"></textarea>
+      </div>
+      
+      <div className='preview-container'>
+        <div className='preview-header'>
+        <p>Preview</p>
+          <p>X</p>
+        </div>
+        <div id='preview'></div>
+      </div>
     </div>
   );
 }
